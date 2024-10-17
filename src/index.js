1f766a535037cc80e1753a8d21e7f121b9dd9129
Kfeliciano94 <Krystalifeliciano@gmail.com>
2024-10-17 13:54:14 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-    new TypeWriter("poem", {
-        strings: "",
-        autostart: true,
-        delay: 1,
+function displayPoem(response) {
+    console.log("poem generator");
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay:55,
         cursor: "",
 
     });
 }
 
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "cd1d8fd1e2ab70a45dfof94bfcfd8t34";
+    let prompt =`User instructions: Generate a poem about ${instructionsInput.value}`;
+    let context ="Following the user instructions, please create a short poem.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+    console.log("generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+   
+
+    axios.get(apiUrl).then(displayPoem).catch(error => {
+        console.error("Error fetching poem:", error);
+    });
+
+ 
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatepoem);
+poemFormElement.addEventListener("submit", generatePoem);
